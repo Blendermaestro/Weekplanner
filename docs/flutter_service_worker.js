be0a587fe3e2704ec19468ddd8e3d2d4478e2cd9
ossi <ossi.littow3@gmail.com>
2025-06-15 18:48:34 +0300

    Deploy dropdown scrolling fix to GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -25,14 +25,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "693204755803eccf49944a9c8a36d058",
+"flutter_bootstrap.js": "b7df3cac1607aed76824a06fbabfc282",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "6c24b039e1dd62c63c6d74cb5ddd1fb9",
 "/": "6c24b039e1dd62c63c6d74cb5ddd1fb9",
-"main.dart.js": "d11f6a33f078f60b231b77a53a9a2615",
+"main.dart.js": "aad56a2f7a1c2b2606415e151656864f",
 "manifest.json": "d2cd796900d6b3fa322a12f4da9f391a",
 "version.json": "5eb9954e12d225b9391cadbfa444cdf1"};
 // The application shell files that are downloaded before a service worker can
